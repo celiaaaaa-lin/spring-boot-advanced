1d7a0fd499f57fdc93c5d936c880008ce8292848
Xinyi(Celia) Lin <linxy128@gmail.com>
2021-02-15 17:45:07 -0500

    Add welcome message in properties file and autowire in /welcome

--- src/main/java/com/springboot/advanced/springbootadvanced/service/WelcomeService.java
+++ src/main/java/com/springboot/advanced/springbootadvanced/service/WelcomeService.java
@@ -1,12 +1,17 @@
 package com.springboot.advanced.springbootadvanced.service;
 
 import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 // Spring to manage this bean and create an instance of it
 // @Component or @Service
 @Component
 public class WelcomeService{
+
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+
     public String receiveWelcomeMessage() {
-        return "Welcome";
+        return welcomeMessage;
     }
 }
