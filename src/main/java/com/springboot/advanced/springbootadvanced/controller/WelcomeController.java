39a2a477195737e473eea48a2ec30c9fe03446c9
Xinyi(Celia) Lin <linxy128@gmail.com>
2021-02-15 18:58:00 -0500

    /dynamic-configuration

--- src/main/java/com/springboot/advanced/springbootadvanced/controller/WelcomeController.java
+++ src/main/java/com/springboot/advanced/springbootadvanced/controller/WelcomeController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class WelcomeController {
 
@@ -12,9 +15,21 @@ public class WelcomeController {
     @Autowired
     private WelcomeService service;
 
+    @Autowired
+    BasicConfiguration configuration;
+
     // localhost:8080/welcome=> welcome message
     @RequestMapping("/welcome")
     public String welcome(){
         return service.receiveWelcomeMessage();
+    }
+
+    @RequestMapping("/dynamic-configuration")
+    public Map dynamicConfig(){
+        Map map = new HashMap();
+        map.put("message", configuration.getMessage());
+        map.put("number", configuration.getNumber());
+        map.put("value", configuration.isValue());
+        return map;
     }
 }
